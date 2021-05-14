2b1eddd56e994e012f6ffdb92139e6d697139f7a
davedwight <dpdwight@gmail.com>
2021-05-14 14:15:16 -0600

    Used axios to console log data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,7 +11,32 @@ const App = () => {
   // sync up with, if any.
 
   //
+
+    const [characters, setCharacters] = useState([])
+    const [currentCharacterName, setCurrentCharacterName] = useState(null)
   
+    const openDetails = name => {
+      setCurrentCharacterName(name)
+    }
+  
+    const closeDetails = () => {
+      setCurrentCharacterName(null)
+    }
+  
+    useEffect(() => {
+      axios('https://swapi.dev/api/people')
+        .then(res => {
+          console.log("res.data:", res.data)
+          // console.log("response: ", res)
+        })
+        // .then(data => {
+        //   console.log("data: ", data)
+        // })
+        .catch(err => {
+          console.log(err)
+        })
+    }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+// import React from 'react';
+// import styled from 'styled-components';
+
+// export default function Character({ info, action, besty }) {
+//     return (
+//       <StyledFriend besty={besty}>
+//         {info.name}
+//         <button onClick={() => action(info.id)}>
+//           See details
+//         </button>
+//       </StyledFriend>
+//     )
+//   }
